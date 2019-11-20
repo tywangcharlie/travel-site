321fb7df93ba8c08d28703e09705058308b4a579
tywangcharlie <twang220@uic.edu>
2019-11-20 07:55:54 -0600

    Added client area feature.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,7 +3,9 @@ import 'lazysizes'
 import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
 import StickyHeader from './modules/StickyHeader'
+import ClientArea from './modules/ClientArea'
 
+new ClientArea();
 
 let stickyHeader = new StickyHeader();
 
