a2bd5a81963d2efcc9a27bc573ded7101dffa32b
tywangcharlie <twang220@uic.edu>
2019-11-20 07:31:21 -0600

    Updated cloud function to check password.

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,27 @@
 exports.handler = function(event, content, callback) {
-    callback(null, {
-        statusCode: 200,
-        body: "Welcome to the secret area"
-    })
+    const secretContent=`
+    <h3>Welcome to the secrete area </h3>
+    <p>Please input the <strong>password.</strong></p>
+    `
+    
+    let body
+
+    if(event.body){
+        body = JSON.parse(event.body)
+    } else {
+        body = {}
+    }
+
+    if(body.password == "javascript") {
+        callback(null, {
+            statusCode: 200,
+            body: secretContent
+        })
+    } else {
+        callback(null, {
+            statusCode: 401,
+        })
+    }
+    
+    
 }
